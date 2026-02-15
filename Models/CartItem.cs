97e14205962c2b7f823e23696b62ea346182e66a
RaghadMjbel1 <ahmadgg112233445@gmail.com>
2026-02-15 14:58:29 +0300

    Update CartItem.cs

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -4,11 +4,7 @@ namespace AfaqMall.Models
     {
         public int Id { get; set; }
         public required string UserId { get; set; }
-
-        // لإبقاء CartController القديم يعمل
         public required Product Product { get; set; }
-        public int? ProductId { get; set; }
-
         public int Quantity { get; set; }
     }
 }
