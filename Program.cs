06b8309b76c72afe8607863098d07133b8e455dd
RaghadMjbel1 <ahmadgg112233445@gmail.com>
2026-02-16 00:45:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
-using AfaqMall.Data;
-using AfaqMall.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Seed data
-var categories = SeedData.GetCategories();
-builder.Services.AddSingleton(categories);
-
 var app = builder.Build();
+app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
 
-app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
