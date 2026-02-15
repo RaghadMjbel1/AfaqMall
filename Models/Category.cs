23b5ec8c266903028dda2e9903aa9def2f296e50
RaghadMjbel1 <ahmadgg112233445@gmail.com>
2026-02-15 13:33:25 +0300

    Create Category.cs

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+namespace AfaqMall.Models
+{
+    public class Category
+    {
+        public int Id { get; set; } // معرف القسم
+        public string Name { get; set; } // اسم القسم (ملابس، أحذية، حقائب، ميك أب)
+    }
+}
