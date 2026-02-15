8d2211cadb27db66daa85828b618074993490518
RaghadMjbel1 <ahmadgg112233445@gmail.com>
2026-02-15 14:25:55 +0300

    Update Category.cs

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,7 +2,7 @@ namespace AfaqMall.Models
 {
     public class Category
     {
-        public int Id { get; set; } // معرف القسم
-        public string Name { get; set; } // اسم القسم (ملابس، أحذية، حقائب، ميك أب)
+        public int Id { get; set; }
+        public required string Name { get; set; } // required لتجنب التحذير
     }
 }
