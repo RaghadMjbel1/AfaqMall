9c631fa9df944df5ee5a56504a28d2a811101930
RaghadMjbel1 <ahmadgg112233445@gmail.com>
2026-02-15 15:11:32 +0300

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,7 +5,6 @@ namespace AfaqMall.Models
         public int Id { get; set; }
         public required string Name { get; set; }
         public decimal Price { get; set; }
-        public int StockQuantity { get; set; }
         public required string Category { get; set; }
         public int CategoryId { get; set; }
         public string ImageUrl { get; set; }
