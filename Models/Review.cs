9a8016e8c6d5cc2a625afcea54a47e54749fd670
RaghadMjbel1 <ahmadgg112233445@gmail.com>
2026-02-15 14:39:56 +0300

    Update Review.cs

--- Models/Review.cs
+++ Models/Review.cs
@@ -5,6 +5,6 @@ namespace AfaqMall.Models
         public int Id { get; set; }
         public required Product Product { get; set; }
         public required string Comment { get; set; }
-        public int Rating { get; set; } // نجوم التقييم
+        public int Rating { get; set; }
     }
 }
